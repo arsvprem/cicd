4b9f51cced8c342515d5c55606ead99b78386715
Prem <arsvprem@gmail.com>
2023-08-09 06:23:20 -0400

    updates

--- 2cicd.py
+++ 2cicd.py
@@ -1,9 +1,13 @@
+import os
 from nornir import InitNornir
 from nornir_scrapli.tasks import send_configs_from_file
 from nornir_utils.plugins.functions import print_result
 from nornir.core.exceptions import NornirExecutionError
 
 nr = InitNornir(config_file="config.yaml")
+nr.inventory.defaults.username = os.getenv("USERNAME")
+nr.inventory.defaults.password = os.getenv("PASSWORD")
+
 
 def random_configs(task):
   task.run(task=send_configs_from_file, file="configtest.txt")
